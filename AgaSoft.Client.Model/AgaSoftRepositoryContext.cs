7f058a226938aa270efdd712e153b201a7d77995
Ermal Halilaga <e.halilaga1@studenti.uniba.it>
2020-10-18 16:48:43 +0200

    first commit

---
+++ AgaSoft.Client.Model/AgaSoftRepositoryContext.cs
@@ -0,0 +1,23 @@
+﻿using AgaSoft.Client.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgaSoft.Client.Model
+{
+    public class AgaSoftRepositoryContext : DbContext
+    {
+        public AgaSoftRepositoryContext(DbContextOptions<AgaSoftRepositoryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Users> Users { get; set; }    
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().ToTable("Users");
+        }
+    }
+}
---
+++ AgaSoft.Client.Model/Entities/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AgaSoft.Client.Model.Entities
+{
+    public class Users
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public int Password { get; set; }
+        public string Email { get; set; }
+
+    }
+}
